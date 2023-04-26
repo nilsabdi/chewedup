1a4972abb2382faa230144ac05be7fe551902d77
nilsabdi <6367969+nilsabdi@users.noreply.github.com>
2023-04-27 01:31:48 +0200

    Video adds flair

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,18 +3,22 @@ import { Inter } from "next/font/google";
 import Scoreboard from "@/components/Scoreboard";
 import { ControlBar } from "@/components/ControlBar";
 import RoundWins from "@/components/RoundWins";
+import YouTubeVideo from "@/components/VideoPlayer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className="bg-gray-100 pb-10 min-h-screen">
-      <div className="p-8">
-        <RoundWins />
+    <main className="bg-gray-100 pb-32 min-h-screen">
+      <div className="grid grid-cols-2">
+        <div className="relative">
+          <YouTubeVideo videoId="jsgxMLVAnu4" />
+        </div>
+        <div className="p-8">
+          <Scoreboard />
+        </div>
       </div>
-      <div className="px-8">
-        <Scoreboard />
-      </div>
+      <RoundWins />
       <ControlBar />
     </main>
   );
